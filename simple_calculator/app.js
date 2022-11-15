afdf1e6538b878e155966c51e745aa8b6835102c
tjddkwh96@naver.com <tjddkwh96@naver.com>
2022-11-15 16:44:59 +0900

    Revert "입력창에 나열되는 문제 해결 #2"
    This reverts commit 2ee9d929c9dfe31feba904e7adb699b5efe4295b.

--- simple_calculator/app.js
+++ simple_calculator/app.js
@@ -2,13 +2,13 @@ const defalutResult = 0;
 let currentResult = defalutResult;
 
 function add() {
-  currentResult = currentResult + parseInt(userInput.value);
-  //1.input값은 무조건 문자열이 되므로 나열이 된다. => 숫자로 바꿔주자
-  outputResult(currentResult, '');
+  currentResult = currentResult + userInput.value;
+  // 1.현재 값과 user가 input한 값을 더한다.
+  outputResult(currentResult,'');
   //2.출력 창에 내가 적은 숫자가 나오도록 해준다.
   //outputResult(result,text)
 }
-
+ 
 addBtn.addEventListener('click', add);
 //버튼 클릭시 add함수 실행
 outputResult(currentResult, calculationDescription);
