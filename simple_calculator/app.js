a454c303cb7e2be1196d2d33ddcdaf7014a91e68
tjddkwh96@naver.com <tjddkwh96@naver.com>
2022-11-15 16:07:28 +0900

    간단한 계산기 작업

---
+++ simple_calculator/app.js
@@ -0,0 +1,15 @@
+const defalutResult = 0;
+let currentResult;
+
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  return result
+}
+
+addBtn.addEventListener('click',add());
+//1.
+outputResult(currentResult, calculationDescription);
+
+
+
