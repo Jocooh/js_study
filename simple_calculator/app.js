ce49f01556af07836eaeaa3aa66b8fef8ccc6542
tjddkwh96@naver.com <tjddkwh96@naver.com>
2022-11-15 16:25:09 +0900

    입력값 출력하기

--- simple_calculator/app.js
+++ simple_calculator/app.js
@@ -1,15 +1,14 @@
 const defalutResult = 0;
-let currentResult;
+let currentResult = defalutResult;
 
-
-function add(num1, num2) {
-  const result = num1 + num2;
-  return result
+function add() {
+  currentResult = currentResult + userInput.value;
+  // 1.현재 값과 user가 input한 값을 더한다.
+  outputResult(currentResult,'');
+  //2.출력 창에 내가 적은 숫자가 나오도록 해준다.
+  //outputResult(result,text)
 }
-
-addBtn.addEventListener('click',add());
-//1.
+ 
+addBtn.addEventListener('click', add);
+//버튼 클릭시 add함수 실행
 outputResult(currentResult, calculationDescription);
-
-
-
